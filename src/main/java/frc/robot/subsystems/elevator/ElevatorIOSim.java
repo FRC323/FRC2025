e1fc82206cfd6f2a571f643e217a482344022638
Aaron Sanders <atsanders@gmail.com>
2025-02-18 15:04:41 -0600

    wpilib update and various elevator things

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems.elevator;
 
+import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.sim.SparkRelativeEncoderSim;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DIOSim;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
@@ -22,14 +25,22 @@ import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 public class ElevatorIOSim implements ElevatorIO {
   private final ElevatorSim elevatorSim;
   private final PIDController controller;
-  private final ElevatorFeedforward feedforward;
+  private final SparkMaxSim leadSpark;
+  private final SparkMaxSim followerSpark;
+  private final SparkRelativeEncoderSim leadEncoder;
+  private final DCMotor motor = DCMotor.getNEO(1).withReduction(ElevatorConstants.gearRatio);
+  // private final ElevatorFeedforward feedforward;
 
   // Mechanism2d visualization
   private final Mechanism2d mechanism2d;
   private final MechanismRoot2d elevatorRoot;
   private final MechanismLigament2d elevatorCarriage;
 
+  // simulating a reverse limit switch
+  // SparkLimitSwitchSim - did not work for me at all
   private final DIOSim bottomLimitSwitch;
+  private final Debouncer limitSwitchDebouncer = new Debouncer(0.1);
+  // private final SparkLimitSwitchSim bottomLimitSwitch;
   private boolean closedLoop = false;
   private double openLoopVoltage = 0.0;
   private double targetHeightInches = 0.0;
@@ -40,36 +51,51 @@ public class ElevatorIOSim implements ElevatorIO {
   private final LoggedNetworkNumber d = new LoggedNetworkNumber("elevatorD", ElevatorConstants.kD);
 
   public ElevatorIOSim() {
-    bottomLimitSwitch = new DIOSim(ElevatorConstants.bottomLimitSwitchChannel);
+    bottomLimitSwitch = new DIOSim(0);
 
     LinearSystem<N2, N1, N2> elevatorSystem =
         LinearSystemId.createElevatorSystem(
-            DCMotor.getNEO(2).withReduction(ElevatorConstants.gearRatio),
-            ElevatorConstants.carriageMass,
-            ElevatorConstants.drumRadius,
-            ElevatorConstants.gearRatio);
+          motor,
+          ElevatorConstants.carriageMass,
+          ElevatorConstants.drumRadius,
+          ElevatorConstants.gearRatio);
 
     elevatorSim =
         new ElevatorSim(
-            elevatorSystem,
-            DCMotor.getNEO(2).withReduction(ElevatorConstants.gearRatio),
-            Units.inchesToMeters(ElevatorConstants.minElevatorHeight),
-            Units.inchesToMeters(ElevatorConstants.maxElevatorHeight),
-            true,
-            0);
+          elevatorSystem,
+          motor,
+          Units.inchesToMeters(ElevatorConstants.minElevatorHeight),
+          Units.inchesToMeters(ElevatorConstants.maxElevatorHeight),
+          false,
+          0);
 
-    TrapezoidProfile.Constraints constraints =
-        new TrapezoidProfile.Constraints(
-            ElevatorConstants.maxVelocity, ElevatorConstants.maxAcceleration);
+    leadSpark =
+        new SparkMaxSim(
+            new SparkMax(ElevatorConstants.leadCanId, MotorType.kBrushless),
+            motor);
+    followerSpark =
+        new SparkMaxSim(
+            new SparkMax(ElevatorConstants.followerCanId, MotorType.kBrushless),
+            motor);
+
+    leadEncoder = leadSpark.getRelativeEncoderSim();
+    // TrapezoidProfile.Constraints constraints =
+    //    new TrapezoidProfile.Constraints(
+    //        ElevatorConstants.maxVelocity, ElevatorConstants.maxAcceleration);
     controller = new PIDController(p.get(), i.get(), d.get());
-    feedforward =
-        new ElevatorFeedforward(
-            ElevatorConstants.kS, ElevatorConstants.kG, ElevatorConstants.kV, ElevatorConstants.kA);
+    // feedforward =
+    //    new ElevatorFeedforward(
+    //        ElevatorConstants.kS, ElevatorConstants.kG, ElevatorConstants.kV,
+    // ElevatorConstants.kA);
 
     // Setup Mechanism2d visualization
     mechanism2d = new Mechanism2d(40, ElevatorConstants.maxElevatorHeight);
     elevatorRoot = mechanism2d.getRoot("Elevator", 20, 0);
     elevatorCarriage = elevatorRoot.append(new MechanismLigament2d("Carriage", 0, 90));
+
+    // bottomLimitSwitch =
+    //    new SparkLimitSwitchSim(
+    //       new SparkMax(ElevatorConstants.leadCanId, MotorType.kBrushless), false);
 
     SmartDashboard.putData("Elevator Simulation", mechanism2d);
   }
@@ -82,8 +108,15 @@ public class ElevatorIOSim implements ElevatorIO {
 
     elevatorSim.update(ElevatorConstants.kDt);
 
-    bottomLimitSwitch.setValue(this.currentHeightInches <= 0);
+    currentHeightInches = Units.metersToInches(elevatorSim.getPositionMeters());
 
+    leadEncoder.setPosition(currentHeightInches);
+
+    // bottomLimitSwitch.setValue(this.currentHeightInches <= 0);
+    boolean isAtBottom = limitSwitchDebouncer.calculate(currentHeightInches <= 0);
+    bottomLimitSwitch.setValue(isAtBottom);
+
+    // if (bottomLimitSwitch.getValue()) {
     if (bottomLimitSwitch.getValue()) {
       elevatorSim.setInput(0);
       elevatorSim.setState(0, 0);
@@ -100,25 +133,23 @@ public class ElevatorIOSim implements ElevatorIO {
         double pidOutput =
             controller.calculate(
                 Units.metersToInches(elevatorSim.getPositionMeters()), inputs.targetHeightPosition);
-        // double ffOutput =
-        // feedforward.calculate(profiledPidController.getSetpoint().velocity);
+        // double ffOutput = feedforward.calculate(controller.getSetpoint().velocity);
         output = pidOutput; // + ffOutput;
 
         elevatorSim.setInput(output);
       }
     } else {
       output = this.openLoopVoltage;
-      // System.out.println("ElevatorIOSim. OpenLoop effort: {" + output + "}");
       elevatorSim.setInput(output);
     }
-
-    currentHeightInches = Units.metersToInches(elevatorSim.getPositionMeters());
 
     inputs.currentHeightPosition = this.currentHeightInches;
     inputs.targetHeightPosition = this.targetHeightInches;
 
     elevatorCarriage.setLength(inputs.currentHeightPosition);
 
+    Logger.recordOutput("Elevator/BottomLimitSwitchPressed", bottomLimitSwitch.getValue());
+    Logger.recordOutput("Elevator/LeadEncoderPosition", leadEncoder.getPosition());
     Logger.recordOutput("Elevator/ControlEffort", output);
   }
 
